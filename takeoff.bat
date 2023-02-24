5ab63280e8485eaf208f5a4bb3f997f47c63e140
brambasiel <bramtech@telenet.be>
2023-02-24 10:03:10 +0100

    correct path

--- takeoff.bat
+++ takeoff.bat
@@ -1,6 +1,12 @@
-set exe=target/release/takeoff.exe
+@echo off
+set root=%LOCALAPPDATA%\nvim\takeoff
+set reldir=%root%\target\release
+set exe=%reldir%\takeoff.exe
+
 if not exist %exe% (
+    pushd %root%
     cargo build --release
+    popd
 )
 call "%exe%" %*
 
