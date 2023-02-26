33950496712226fa763f5cb0a8f62158b58dd9c2
= <=>
2023-02-26 17:50:37 +0100

    better scripts

--- takeoff.bat
+++ takeoff.bat
@@ -1,1 +1,12 @@
-cargo run --release -- %*
+@echo off
+
+set root=%LOCALAPPDATA%\nvim\takeoff
+set reldir=%root%\target\release
+set exe=%reldir%\takeoff.exe
+
+if not exist %exe% (
+    pushd %root%
+    cargo build --release
+    popd
+)
+call "%exe%" %*
--- takeoff.sh
+++ takeoff.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-cargo run release -- $@
+DIR="$HOME/.config/nvim/takeoff/target/release"
+EXE="takeoff"
+if ! [ -f "$DIR/$EXE" ]; then
+    OLD=${pwd}
+    cd $DIR
+    cargo build --release
+    cd $OLD
+fi
+
+exec $DIR/$EXE $@
