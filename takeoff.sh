fe9a8217e3696daa9c195104544e9f47247ac14a
bramtechs <bramtech@telenet.be>
2023-03-13 15:52:54 +0100

    simpler linux script

--- takeoff.sh
+++ takeoff.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-DIR="$HOME/.config/nvim/takeoff/target/release"
-EXE="takeoff"
-if ! [ -f "$DIR/$EXE" ]; then
-    OLD=${pwd}
-    cd $DIR
-    cargo build --release
-    cd $OLD
-fi
-
-exec $DIR/$EXE $@
+OLD=${pwd}
+cd "$HOME/.config/nvim/takeoff/target/release"
+cargo run --release -- $@
+cd $OLD
